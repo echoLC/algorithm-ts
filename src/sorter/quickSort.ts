3dd343e48c1ae34d5b7a13580833b314be5e8305
luochao <luochao@joyy.sg>
2021-08-21 11:26:46 +0800

    feat: 添加了快速排序算法

---
+++ src/sorter/quickSort.ts
@@ -0,0 +1,20 @@
+// 快速排序
+export default function quickSort(arr: Array<number>): Array<number> {
+  if (arr.length <= 1) return arr
+
+  const pivotIndex = Math.floor(arr.length / 2)
+  const pivot = arr[pivotIndex]
+
+  const left: number[] = []
+  const right: number[] = []
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < pivot) {
+      left.push(arr[i])
+    } else {
+      right.push(arr[i])
+    }
+  }
+
+  return quickSort(left).concat([pivot], quickSort(right))
+}
---
+++ src/sorter/threeWayQuickSort.ts
@@ -0,0 +1,43 @@
+// 三向切分快速排序
+export default function threeWayQuickSort(
+  arr: Array<number>,
+  left: number,
+  right: number
+): Array<number> {
+  let lt = 0
+  let i = 0
+  let gt = 0
+  let v = 0
+
+  if (left >= right) return arr
+
+  v = arr[left]
+  lt = left
+  i = left + 1
+  gt = right
+
+  while (i <= gt) {
+    if (arr[i] < v) {
+      swap(arr, lt, i)
+      lt++
+      i++
+    } else if (arr[i] > v) {
+      swap(arr, i, gt)
+      gt--
+      i++
+    } else {
+      i++
+    }
+  }
+
+  threeWayQuickSort(arr, left, lt - 1)
+  threeWayQuickSort(arr, gt + 1, right)
+
+  return arr
+}
+
+function swap(arr: Array<number>, i: number, j: number) {
+  let tmp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = tmp
+}
