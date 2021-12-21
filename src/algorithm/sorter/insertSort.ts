429ad402a839b937cf0c9320e99cd2de4c906937
luochao <luochao@joyy.com>
2021-12-21 21:40:56 +0800

    fix: 优化了插入排序

--- src/algorithm/sorter/insertSort.ts
+++ src/algorithm/sorter/insertSort.ts
@@ -1,13 +1,20 @@
 // 插入排序
 export default function insertSort(data: Array<number> | unknown): Array<number> {
   if (!Array.isArray(data)) throw new Error('Input muse be an Array')
+  if (data.length <= 1) return data
 
   for (let i = 0; i < data.length; i++) {
-    for (let j = i; j > 0 && data[j] < data[j - 1]; j--) {
-      let min = data[j]
-      data[j] = data[j - 1]
-      data[j - 1] = min
+    const value = data[i]
+    let j = i - 1
+    // 查找插入的位置
+    for (; j >= 0; j--) {
+      if (data[j] > value) {
+        data[j + 1] = data[j] // 数据移动
+      } else {
+        break
+      }
     }
+    data[j + 1] = value // 插入数据
   }
   return data
 }
