4aa5ed5679737c81f4aa66e4b4be70f82bb1f6d8
luochao <luochao@joyy.com>
2023-02-03 08:41:14 +0800

    fix: 完善链表反转

--- src/algorithm/linkList/reverseList.ts
+++ src/algorithm/linkList/reverseList.ts
@@ -1,14 +1,17 @@
 // 反转单链表
 const reverseList = (head: ListNode | null) => {
-  if (head == null || head.next == null) return head
+  if (head == null || head.next == null) return head;
 
-  let pre: ListNode | null = null
-  let cur: ListNode | null = head
-  while (cur) {
-    let next = cur.next
-    cur.next = pre
-    pre = cur
-    cur = next
+  let pre: ListNode | null = head;
+  let cur: ListNode | null = null;
+
+  while (pre != null) {
+    const next = pre.next;
+
+    pre.next = cur;
+    cur = pre;
+    pre = next;
   }
-  return pre
-}
+
+  return cur;
+};
