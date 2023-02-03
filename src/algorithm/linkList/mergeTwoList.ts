d7f74d9db4b1345dc80af256550e8f48af631526
luochao <luochao@joyy.com>
2023-02-03 08:36:31 +0800

    fix: 完善合并有序链表

--- src/algorithm/linkList/mergeTwoList.ts
+++ src/algorithm/linkList/mergeTwoList.ts
@@ -1,25 +1,41 @@
-// 合并两个有序的列表
+// 合并两个有序的列表 -- 迭代
 function mergeTwoList(list1: ListNode | null, list2: ListNode | null) {
-  if (list1 == null) return list2
-  if (list2 == null) return list1
+  if (list1 == null) return list2;
+  if (list2 == null) return list1;
 
-  let dummyHead = new ListNode()
-  let cur = dummyHead
-  while (list1 && list2) {
+  const dummyHead = new ListNode();
+  let cur = dummyHead;
+
+  while (list1 != null && list2 != null) {
     if (list1.val < list2.val) {
-      cur.next = list1
-      cur = cur.next
-      list1 = list1.next
+      cur.next = list1;
+      list1 = list1.next;
     } else {
-      cur.next = list2
-      cur = cur.next
-      list2 = list2.next
+      cur.next = list2;
+      list2 = list2.next;
     }
+
+    cur = cur.next;
   }
+
+  cur.next = list1 == null ? list2 : list1;
+
+  return dummyHead.next;
+}
+
+// 合并两个有序的列表 -- 递归
+function mergeTwoList1(list1: ListNode | null, list2: ListNode | null) {
   if (list1 == null) {
-    cur.next = list2
+    return list2;
+  } else if (list2 == null) {
+    return list1;
+  } else if (list1.val < list2.val) {
+    list1.next = mergeTwoList1(list1.next, list2);
+
+    return list1;
   } else {
-    cur.next = list1
+    list2.next = mergeTwoList1(list1, list2.next);
+
+    return list2;
   }
-  return dummyHead.next
 }
