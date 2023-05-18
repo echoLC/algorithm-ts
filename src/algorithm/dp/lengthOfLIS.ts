147e95e3dc26e6a2badc9b06a8de5c21d43ac575
luochao <luochao@joyy.com>
2023-05-18 08:50:27 +0800

    feat: 增加'最长递增子序列'题解

---
+++ src/algorithm/dp/lengthOfLIS.ts
@@ -0,0 +1,13 @@
+export default function lengthOfLIS(nums: number[]) {
+  let dp: number[] = new Array(nums.length).fill(1);
+
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+
+  return Math.max(...dp);
+}
