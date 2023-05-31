b7eb9571889d53bb17b8474e2e9765897586928d
luochao <luochao@joyy.com>
2023-05-31 09:41:13 +0800

    fix: 完善部分算法题的解答

--- src/algorithm/linkList/addTwoNumbers.ts
+++ src/algorithm/linkList/addTwoNumbers.ts
@@ -1,33 +1,36 @@
 // 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字
 
-function addTwoNumbers (l1: ListNode | null, l2: ListNode | null) {
-  const dummy = new ListNode(-1)
-  let current = dummy
-  let carry = 0
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null) {
+  const dummy = new ListNode(-1);
+  let current = dummy;
 
-  while (l1 != null || l2 != null) {
-    const value1 = l1 == null ? 0 : l1.val
-    const value2 = l2 == null ? 0 : l2.val
+  let carry = 0;
 
-    let sum = value1 + value2 + carry
-    carry = parseInt(`${sum / 10}`)
-    sum = sum % 10
+  while (l1 || l2) {
+    const val1 = l1 ? l1.val : 0;
+    const val2 = l2 ? l2.val : 0;
+    const sum = val1 + val2 + carry;
 
-    const node = new ListNode(sum)
-    current.next = node
+    const nodeVal = sum % 10;
+    carry = parseInt(`${sum / 10}`);
+
+    const node = new ListNode(nodeVal);
+    current.next = node;
 
     if (l1) {
-      l1 = l1.next
+      l1 = l1.next;
     }
 
     if (l2) {
-      l2 = l2.next
+      l2 = l2.next;
     }
+
+    current = current.next;
   }
 
-  if (carry === 1) {
-    current.next = new ListNode(carry)
+  if (carry > 0) {
+    current.next = new ListNode(carry);
   }
 
-  return dummy.next
+  return dummy.next;
 }
--- src/algorithm/linkList/partition.ts
+++ src/algorithm/linkList/partition.ts
@@ -1,28 +1,28 @@
 // 给你一个链表的头节点 head 和一个特定值 x ，请你对链表进行分隔，使得所有 小于 x 的节点都出现在 大于或等于 x 的节点之前
-function partition (head: ListNode | null, x: number) {
-  if (head == null || head.next == null) return head
+function partition(head: ListNode | null, x: number) {
+  if (head == null || head.next == null) return head;
 
-  const dummy1 = new ListNode(-1)
-  const dummy2 = new ListNode(-1)
+  const dummy1 = new ListNode(-1);
+  const dummy2 = new ListNode(-1);
 
-  let left = dummy1
-  let right = dummy2
+  let left = dummy1;
+  let right = dummy2;
 
-  while(head) {
+  while (head) {
     if (head.val >= x) {
-      right.next = head
-      right = right.next
+      right.next = head;
+      right = right.next;
     } else {
-      left.next = head
-      left = left.next
+      left.next = head;
+      left = left.next;
     }
 
-    const next = head.next
-    head.next = null
-    head = next
+    head = head.next;
   }
 
-  left.next = dummy2.next
+  // 避免形成环
+  right.next = null;
+  left.next = dummy2.next;
 
-  return dummy1.next
+  return dummy1.next;
 }
