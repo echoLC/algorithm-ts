ae42a817c8e5f5dd1c69e2861648bf08787856b4
luochao <luochao@joyy.com>
2023-06-06 08:39:05 +0800

    feat: 添加'差分数组'解答

---
+++ src/algorithm/array/getModifiedArray.ts
@@ -0,0 +1,53 @@
+// 差分数组
+class Difference {
+  private diff: number[] = [];
+
+  constructor(list: number[]) {
+    this.init(list);
+  }
+
+  init(list: number[]) {
+    if (!list.length) return;
+
+    this.diff = new Array(list.length);
+    this.diff[0] = list[0];
+
+    for (let i = 1; i < list.length; i++) {
+      this.diff[i] = list[i] - list[i - 1];
+    }
+  }
+
+  increment(i, j, val) {
+    this.diff[i] += val;
+
+    if (j + 1 < this.diff.length) {
+      this.diff[j + 1] -= val;
+    }
+  }
+
+  result() {
+    const res = new Array(this.diff.length);
+
+    res[0] = this.diff[0];
+
+    for (let i = 1; i < this.diff.length; i++) {
+      res[i] = res[i - 1] + this.diff[i];
+    }
+
+    return res;
+  }
+}
+
+function getModifiedArray(length: number, updates: number[][]) {
+  const list = new Array(length).fill(0);
+
+  const diff = new Difference(list);
+
+  for (let i = 0; i < updates.length; i++) {
+    const update = updates[i];
+    const [left, right, val] = update;
+    diff.increment(left, right, val);
+  }
+
+  return diff.result();
+}
