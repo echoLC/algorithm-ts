01ba3e8823d65b87b1fec4e259147b5b57c084c2
luochao <luochao@joyy.com>
2023-06-09 09:50:57 +0800

    fix: 完善'填充右节点'的解答

--- src/algorithm/tree/connect.ts
+++ src/algorithm/tree/connect.ts
@@ -1,26 +1,23 @@
 class TreeNodeWithNext extends TreeNode {
-  next?: TreeNode | null
+  next?: TreeNode | null;
 }
 
 // 填充每个节点的下一个右侧节点指针
 function connect(root: TreeNodeWithNext | null) {
-  if (root == null) return null
+  if (root == null) return null;
 
-  const traverse = (node1: TreeNodeWithNext | null, node2: TreeNodeWithNext | null)=> {
-    if (node1 == null || node2 == null) return 
+  const traverse = (node1: TreeNodeWithNext | null, node2: TreeNodeWithNext | null) => {
+    if (node1 == null || node2 == null) return;
 
-    node1.next = node2
+    node1.next = node2;
 
-    // 同一父节点的左右节点连接
-    traverse(node1.left, node1.right)
-    traverse(node2.left, node2.right)
+    traverse(node1.left, node1.right);
+    traverse(node2.left, node2.right);
+    // 不同父节点的连接
+    traverse(node1.right, node2.left);
+  };
 
-    // 不同父节点的左右节点连接
-    traverse(node1.right, node2.left)
-  }
+  traverse(root.left, root.right);
 
-  traverse(root.left, root.right)
-
-
-  return root
+  return root;
 }
